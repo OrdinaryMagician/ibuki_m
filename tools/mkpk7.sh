4bc8eaaa9662ebcd3dc9a79f32e623e00c46a6d7
Marisa Kirisame <marisa@sayachan.org>
2021-11-18 21:30:21 +0100

    More stuff, I suppose.

--- tools/mkpk7.sh
+++ tools/mkpk7.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 WORKDIR=$(readlink -f $0 | sed 's/\(ibuki_m\)\(.*\)/\1/')
-pushd "$WORKDIR"
-7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=off -x@tools/excl.lst -up0q0r2x2y2z1w2 ../ibuki${1}_m.pk7 '../swwmgzlib_m/*' '*'
+LIBDIR=${WORKDIR}/../swwmgzlib_m
+DESTFILE=${WORKDIR}/../ibuki${1}_m.pk7
+mkdir -p /tmp/tempwork
+pushd /tmp/tempwork
+cp -ar $LIBDIR/* .
+cp -ar $WORKDIR/* .
+7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=off -x@tools/excl.lst -up0q0r2x2y2z1w2 $DESTFILE '*'
 popd
+rm -rf /tmp/tempwork
